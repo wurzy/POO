c40afc3cae5b540a5cbe20c48ac5079a48619a57
Válter <47797461+wurzy@users.noreply.github.com>
2019-04-26 03:22:43 +0100

    Delete AluguerComparator.java

--- AluguerComparator.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class AluguerComparator implements Comparator<Aluguer> {
-    public int compare(Aluguer al1, Aluguer al2) {
-        return Integer.compare(al1.getAluguerID(),al2.getAluguerID());
-    }
-}
-
